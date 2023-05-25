e3323f0ce810a6a25d26ffb0a423fc1dace8d585
Jämes Ménétrey <james@menetrey.org>
2023-05-26 00:55:37 +0200

    Add more recipes for the chapter 2.

---
+++ ConcurrencyInCSharpCookbook/Chapter2AsyncBasics/Recipe2P9HandlingExceptionInAsyncVoid.cs
@@ -0,0 +1,37 @@
+﻿using Nito.AsyncEx;
+
+namespace ConcurrencyInCSharpCookbook.Chapter2AsyncBasics
+{
+    [TestClass]
+    public class Recipe2P9HandlingExceptionInAsyncVoid
+    {
+
+        [TestMethod]
+        public void WorkAroundTwoWithCustomSynchronizationContext()
+        {
+            try
+            {
+                // The AsyncEx library provides the type AsyncContext, which has its own SynchronizationContext,
+                // TaskScheduler and TaskFactory, which executes synchronously the asynchronous delegate, and
+                // bubble up any occurring exception.
+                // The scheduled delegate is executed on a thread pool thread.
+                AsyncContext.Run(ThrowVoidAsync);
+                Assert.Fail();
+            }
+            catch (ExpectedException _)
+            {
+                return;
+            }
+
+            Assert.Fail();
+        }
+
+        private async void ThrowVoidAsync()
+        {
+            await Task.Yield();
+            throw new ExpectedException();
+        }
+
+        private class ExpectedException : Exception { }
+    }
+}
