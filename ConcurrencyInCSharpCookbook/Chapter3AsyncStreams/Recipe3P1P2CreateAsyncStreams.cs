def08f3a639d0b4f77478dff5ebf7fcee31dd671
Jämes Ménétrey <james@menetrey.org>
2023-06-21 23:10:54 +0200

    Start the chapter on async streams.

---
+++ ConcurrencyInCSharpCookbook/Chapter3AsyncStreams/Recipe3P1P2CreateAsyncStreams.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+
+namespace ConcurrencyInCSharpCookbook.Chapter3AsyncStreams
+{
+    [TestClass]
+    public class Recipe3P1P2CreateAsyncStreams
+    {
+        /// <summary>
+        /// An asynchronous stream function may use <c>async</c>, <c>await</c>, and <c>yield</c> instructions.
+        /// The caller consumes these values using <c>await foreach</c> instruction.
+        /// </summary>
+        [TestMethod]
+        public async Task ReadAsyncStream()
+        {
+            var multiples = new List<int>();
+
+            await foreach (var n in MultipleOfAsync(3, 10))
+            {
+                multiples.Add(n);
+            }
+
+            multiples.Should().Equal(3, 6, 9);
+        }
+
+        [TestMethod]
+        public async Task ReadAsyncStreamWithoutContextCapture()
+        {
+            var multiples = new List<int>();
+
+            // The returned task after the "in" keyword can be configured as a regular task.
+            await foreach (var n in MultipleOfAsync(3, 10).ConfigureAwait(false))
+            {
+                multiples.Add(n);
+                // The body of the loop may be async as well.
+                await Task.Delay(0).ConfigureAwait(false);
+            }
+
+            multiples.Should().Equal(3, 6, 9);
+        }
+
+        private async IAsyncEnumerable<int> MultipleOfAsync(int n, int cap)
+        {
+            // May pull some resources on async I/O and buffer some elements to return.
+            // The following is a synthetic example.
+            int currentValue = n;
+
+            while (currentValue < cap)
+            {
+                yield return currentValue;
+                currentValue += n;
+                await Task.Yield();
+            }
+        }
+    }
+}
---
+++ ConcurrencyInCSharpCookbook/Chapter3AsyncStreams/Recipe3P3AsyncLinq.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+
+namespace ConcurrencyInCSharpCookbook.Chapter3AsyncStreams
+{
+    [TestClass]
+    public class Recipe3P3AsyncLinq
+    {
+        /// <summary>
+        /// The usual LINQ methods are compatible with streams, as long as the predicate is synchronous.
+        /// For async predicates, check out <see cref="ConsumeAsyncLinqWithAsyncPredicate"/>.
+        /// </summary>
+        [TestMethod]
+        public async Task ConsumeAsyncLinqWithSyncPredicate()
+        {
+            var values = MultipleOfAsync(3, 10).Where(x => x % 2 == 0);
+
+            (await values.ToListAsync()).Should().Equal(6);
+        }
+
+        /// <summary>
+        /// We need to use the NuGet package "System.Linq.Async" to use async predicates on streams.
+        /// </summary>
+        [TestMethod]
+        public async Task ConsumeAsyncLinqWithAsyncPredicate()
+        {
+            var values = MultipleOfAsync(3, 10).WhereAwait(PredicateAsync);
+
+            (await values.ToListAsync()).Should().Equal(6);
+        }
+
+        private async IAsyncEnumerable<int> MultipleOfAsync(int n, int cap)
+        {
+            // May pull some resources on async I/O and buffer some elements to return.
+            // The following is a synthetic example.
+            int currentValue = n;
+
+            while (currentValue < cap)
+            {
+                yield return currentValue;
+                currentValue += n;
+                await Task.Yield();
+            }
+        }
+
+        private async ValueTask<bool> PredicateAsync(int x)
+        {
+            await Task.Yield();
+            return x % 2 == 0;
+        }
+    }
+}
