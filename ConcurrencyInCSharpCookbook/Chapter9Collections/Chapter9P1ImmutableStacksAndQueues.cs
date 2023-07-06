269c0939d97b304e3149ce955628e439a36d0bc0
Jämes Ménétrey <james@menetrey.org>
2023-07-07 00:27:49 +0200

    Add imutable stacks, queues and lists.

---
+++ ConcurrencyInCSharpCookbook/Chapter9Collections/Chapter9P1ImmutableStacksAndQueues.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Immutable;
+using FluentAssertions;
+
+namespace ConcurrencyInCSharpCookbook.Chapter9Collections;
+
+[TestClass]
+public class Chapter9P1ImmutableStacksAndQueues
+{
+    [TestMethod]
+    public void UsingImmutableStack()
+    {
+        var stack = ImmutableStack<int>.Empty;
+        stack = stack.Push(1);
+        stack = stack.Push(2);
+        stack = stack.Push(3);
+        stack.Should().Equal(3, 2, 1);
+        
+        stack = stack.Pop(out var popped);
+        stack.Should().Equal(2, 1);
+        popped.Should().Be(3);
+    }
+
+    [TestMethod]
+    public void UsingImmutableQueue()
+    {
+        var queue = ImmutableQueue<int>.Empty;
+        queue = queue.Enqueue(1);
+        queue = queue.Enqueue(2);
+        queue = queue.Enqueue(3);
+        queue.Should().Equal(1, 2, 3);
+
+        queue = queue.Dequeue(out var dequeued);
+        queue.Should().Equal(2, 3);
+        dequeued.Should().Be(1);
+    }
+}
---
+++ ConcurrencyInCSharpCookbook/Chapter9Collections/Chapter9P2ImmutableLists.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Immutable;
+using FluentAssertions;
+
+namespace ConcurrencyInCSharpCookbook.Chapter9Collections;
+
+[TestClass]
+public class Chapter9P2ImmutableLists
+{
+    /// <summary>
+    /// Watch out the complexity of <see cref="ImmutableList"/>, which is different from <see cref="List{T}"/>.
+    /// The immutable variant maintains a binary tree to maximise the reuse of the memory.
+    /// </summary>
+    [TestMethod]
+    public void UsingImmutableList()
+    {
+        var list = ImmutableList<int>.Empty;
+        list = list.Add(1); // Complexity: O(log n).
+        list = list.Insert(0, 2); // Complexity: O(log n).
+        list.Should().Equal(2, 1); // Complexity: O(n).
+        list[1].Should().Be(1); // Complexity: O(log n).
+        list = list.RemoveAt(1); // Complexity O(log n).
+
+        list.Should().Equal(2);
+    }
+}
