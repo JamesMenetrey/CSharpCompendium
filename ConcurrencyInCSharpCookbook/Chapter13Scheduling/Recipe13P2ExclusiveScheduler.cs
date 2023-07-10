9319100bfc6c8b2a4c038a97300ad7013e4343a1
Jämes Ménétrey <james@menetrey.org>
2023-07-11 00:14:24 +0200

    Complementary recipes for chapter 13 (scheduling).

---
+++ ConcurrencyInCSharpCookbook/Chapter13Scheduling/Recipe13P2ExclusiveScheduler.cs
@@ -0,0 +1,83 @@
+﻿using FluentAssertions;
+
+namespace ConcurrencyInCSharpCookbook.Chapter13Scheduling;
+
+[TestClass]
+public class Recipe13P2ExclusiveScheduler
+{
+    /// <summary>
+    /// The task scheduler <see cref="ConcurrentExclusiveSchedulerPair"/> includes two <see cref="TaskScheduler"/>s.
+    /// The first one, <see cref="ConcurrentExclusiveSchedulerPair.ConcurrentScheduler"/>, enables to execute tasks
+    /// concurrently, as long as <see cref="ConcurrentExclusiveSchedulerPair.ExclusiveScheduler"/> is not executing
+    /// a task. The second one, <see cref="ConcurrentExclusiveSchedulerPair.ExclusiveScheduler"/>, only executes
+    /// on task at a time.
+    /// </summary>
+    [TestMethod]
+    public async Task UseConcurrentExclusiveScheduler()
+    {
+        var scheduler = new ConcurrentExclusiveSchedulerPair();
+        var concurrent = scheduler.ConcurrentScheduler;
+        var exclusive = scheduler.ExclusiveScheduler;
+
+        await MustExecuteConcurrently(new TaskFactory(concurrent));
+        await MustExecuteSequentially(new TaskFactory(exclusive));
+    }
+
+    private async Task MustExecuteConcurrently(TaskFactory factory)
+    {
+        var array1 = new int[1000];
+        var array2 = new int[1000];
+
+        var task1 = factory.StartNew(() =>
+        {
+            for (var i = 0; i < array1.Length; i++)
+            {
+                ++array1[i];
+                // In the case we are in the last iteration, the tasks2 must have already started.
+                if (i == array1.Length - 1)
+                {
+                    array2[0].Should().BeGreaterThan(0);
+                }
+            }
+        });
+        
+        var task2 = factory.StartNew(() =>
+        {
+            for (var i = 0; i < array1.Length; i++)
+            {
+                ++array2[i];
+            }
+        });
+
+        await Task.WhenAll(task1, task2);
+    }
+    
+    private async Task MustExecuteSequentially(TaskFactory factory)
+    {
+        var array1 = new int[1000];
+        var array2 = new int[1000];
+
+        var task1 = factory.StartNew(() =>
+        {
+            for (var i = 0; i < array1.Length; i++)
+            {
+                ++array1[i];
+                // In the case we are in the last iteration, the tasks2 must NOT have already started.
+                if (i == array1.Length - 1)
+                {
+                    array2[0].Should().Be(0);
+                }
+            }
+        });
+        
+        var task2 = factory.StartNew(() =>
+        {
+            for (var i = 0; i < array1.Length; i++)
+            {
+                ++array2[i];
+            }
+        });
+
+        await Task.WhenAll(task1, task2);
+    }
+}
---
+++ ConcurrencyInCSharpCookbook/Chapter13Scheduling/Recipe13P3ThrottlingCpuBoundWithTaskScheduler.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using FluentAssertions;
+
+namespace ConcurrencyInCSharpCookbook.Chapter13Scheduling;
+
+[TestClass]
+public class Recipe13P3ThrottlingCpuBoundWithTaskScheduler
+{
+    /// <summary>
+    /// The task scheduler <see cref="ConcurrentExclusiveSchedulerPair" /> can be used to throttle the execution of
+    /// async code. Note that this kind of throttling only throttles code whjile it is executing,. It's quite different
+    /// than the kind of logical throttling covered in <see cref="Chapter12Synchronization.Chapter12P5Throttling" />.
+    /// In particular, async code is not considered to be executing while it is awaiting an operation.
+    /// Tis scheduler throttles executing code; other throttling, such as <see cref="SemaphoreSlim" />, throttles at a
+    /// higher level (i.e., an entire async method).
+    /// </summary>
+    [TestMethod]
+    public void ThrottleCpuBoundComputationUsingTaskScheduler()
+    {
+        var throttledScheduler = new ConcurrentExclusiveSchedulerPair(TaskScheduler.Default, 2);
+        var array = new int[1000];
+        var options = new ParallelOptions { TaskScheduler = throttledScheduler.ConcurrentScheduler };
+
+        var limitedStopWatch = new Stopwatch();
+        limitedStopWatch.Start();
+        Parallel.For(0, array.Length, options, index => ++array[index]);
+        limitedStopWatch.Stop();
+
+        array.Sum().Should().Be(1000);
+
+        var fullStopWatch = new Stopwatch();
+        fullStopWatch.Start();
+        Parallel.For(0, array.Length, index => ++array[index]);
+        fullStopWatch.Stop();
+
+        array.Sum().Should().Be(2000);
+
+        if (Environment.ProcessorCount > 2) fullStopWatch.Elapsed.Should().BeLessThan(limitedStopWatch.Elapsed);
+    }
+}
