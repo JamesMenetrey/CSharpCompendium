34dd446c915077f08c36a35ac41d173172322b02
Jämes Ménétrey <james@menetrey.org>
2023-07-23 02:45:38 +0200

    Add the recipes for the implicit state and sync/async identical code.

---
+++ ConcurrencyInCSharpCookbook/Chapter14Scenarios/Recipe14P4ImplicitState.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Immutable;
+using FluentAssertions;
+
+namespace ConcurrencyInCSharpCookbook.Chapter14Scenarios;
+
+[TestClass]
+public class Recipe14P4ImplicitState
+{
+    /// <summary>
+    /// The type <see cref="AsyncLocal{T}" /> enables an object to have a state which lives on a logical context.
+    /// Beware that <see cref="AsyncLocal{T}" /> must store immutable objects only. When updated, the existing value
+    /// must be overwritten. One can typically create an helper class to ensure the stored data is immutable
+    /// and updated correctly.
+    /// <see cref="AsyncLocal{T}" /> supersedes the values bound to a particular thread (notably done using the attribute
+    /// <see cref="STAThreadAttribute" />).
+    /// </summary>
+    [TestMethod]
+    public async Task UseImplicitStatesWithAsyncContext()
+    {
+        var counter = 0;
+        var controlOfExecution = 0;
+        var state = new ImplicitStateDemo<int>();
+        state.Current.Should().BeEmpty();
+
+        // The multiple tasks demonstrate that the state is locale for each task (and not shared).
+        var tasks = Enumerable.Repeat(0, 10).Select(_ => Task.Run(() =>
+        {
+            var value1 = Interlocked.Increment(ref counter);
+            var value2 = Interlocked.Increment(ref counter);
+
+            state.Enqueue(value1);
+            state.Current.Should().HaveCount(1);
+            state.Current.Peek().Should().Be(value1);
+
+            state.Enqueue(value2);
+            state.Current.Should().HaveCount(2);
+            state.Current.Peek().Should().Be(value1);
+
+            state.Dequeue().Should().Be(value1);
+            state.Current.Should().HaveCount(1);
+
+            state.Dequeue().Should().Be(value2);
+            state.Current.Should().BeEmpty();
+
+            Interlocked.Increment(ref controlOfExecution);
+        }));
+
+        await Task.WhenAll(tasks);
+
+        controlOfExecution.Should().Be(10);
+    }
+
+    /// <summary>
+    /// Helper class which ensures that the instance of <see cref="AsyncLocal{T}"/> is properly assigned and updated.
+    /// </summary>
+    private class ImplicitStateDemo<T>
+    {
+        private readonly AsyncLocal<ImmutableQueue<T>> _state = new();
+
+        public ImmutableQueue<T> Current => _state.Value ?? ImmutableQueue<T>.Empty;
+
+        public void Enqueue(T value)
+        {
+            _state.Value = Current.Enqueue(value);
+        }
+
+        public T Dequeue()
+        {
+            _state.Value = Current.Dequeue(out var value);
+            return value;
+        }
+    }
+}
---
+++ ConcurrencyInCSharpCookbook/Chapter14Scenarios/Recipe14P5IdenticalSyncAndAsyncCode.cs
@@ -0,0 +1,77 @@
+﻿using FluentAssertions;
+
+namespace ConcurrencyInCSharpCookbook.Chapter14Scenarios;
+
+/// <summary>
+/// There is no perfect solution. Calling asynchronous code from synchronous may deadlock, while calling synchronous
+/// code from asynchronous is not efficient.
+/// One potential solution is called the Boolean Argument Hack, which instructs to have a private core function that
+/// contains the logic, and two other methods to expose an synchronous and an asynchronous API, reusing the code function.
+/// The private core function has an argument stating whether it must be executed synchronously.
+/// </summary>
+[TestClass]
+public class Recipe14P5IdenticalSyncAndAsyncCode
+{
+    [TestMethod]
+    public async Task CallAsynchronously()
+    {
+        var demo = new SyncAndAsyncDemo();
+        (await demo.DelayAndReturnValueAsync(42)).Should().Be(42);
+    }
+    
+    [TestMethod]
+    public void CallSynchronously()
+    {
+        var demo = new SyncAndAsyncDemo();
+        demo.DelayAndReturnValue(42).Should().Be(42);
+    }
+
+    private class SyncAndAsyncDemo
+    {
+        /// <summary>
+        /// The synchronous API, which instructs the core function to act synchronously (using the boolean parameter).
+        /// </summary>
+        public int DelayAndReturnValue(int value)
+        {
+            var normallyCompletedTask = DelayAndReturnValueCore(value, true);
+            Assert.IsTrue(normallyCompletedTask.IsCompleted);
+
+            // We can safely using the GetAwaiter and GetResult without a deadlock occurring, because the task has
+            // already completed. Therefore, a deadlock cannot occur.
+            return normallyCompletedTask.GetAwaiter().GetResult();
+        }
+
+        /// <summary>
+        /// The asynchronous API, which allows the core function to behave asynchronously (using the boolean parameter).
+        /// </summary>
+        public Task<int> DelayAndReturnValueAsync(int value)
+        {
+            return DelayAndReturnValueCore(value, false);
+        }
+
+        /// <summary>
+        /// The core function, with the code that is shared between the async and sync API.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="sync">Indicates whether the function must complete synchronously.</param>
+        /// <remarks>
+        /// When the boolean parameter is set to <c>true</c>, the function must absolutely execute synchronously, as
+        /// the program may deadlock otherwise. The completeness of the task is checked when using the public sync API.
+        /// </remarks>
+        private async Task<int> DelayAndReturnValueCore(int value, bool sync)
+        {
+            // Some operations before for asynchronous code...
+            var timeToWait = value % 10;
+
+            // The synchronous call if requested.
+            if (sync)
+                // We use the synchronous API for awaiting.
+                Thread.Sleep(timeToWait);
+            else
+                // We use the asynchronous API for awaiting.
+                await Task.Delay(timeToWait);
+
+            return value;
+        }
+    }
+}
